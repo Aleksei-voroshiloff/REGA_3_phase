5ba7e87dbe6a933ac315666692d6336771022478
Aleksei-voroshiloff <alexey909@gmail.com>
2025-01-26 12:26:19 +0300

    rega

---
+++ server/db/seeders/20250114073046-initial-seed.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+
+'use strict';
+
+const bcrypt = require('bcrypt');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          name: 'Алексей',
+          email: 'a@aa.com',
+          password: bcrypt.hashSync('1111qQ!', 10),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
